3e8bc3c78b5e57958158ff36b13a9887103c8971
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-03-12 17:46:25 +0000

    salvar a pasta 4

---
+++ Pasta_aula4/dois.py
@@ -0,0 +1,9 @@
+a = int(input("Digite um valor: "))
+b = int(input("Digite outro valor: "))
+
+if a > b:
+    print(f"Maior: {a}")
+elif b > a:
+    print(f"Maior: {b}")
+else:
+    print("Números iguais")

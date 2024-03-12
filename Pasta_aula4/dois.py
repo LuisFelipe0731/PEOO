e68540c7200a198b53e9988d6960cd88996e66cb
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-03-12 17:57:39 +0000

    salvar aula 4

--- Pasta_aula4/dois.py
+++ Pasta_aula4/dois.py
@@ -1,9 +1,14 @@
-a = int(input("Digite um valor: "))
-b = int(input("Digite outro valor: "))
+""" Comentario legal """
+def Num_Menor(x,y):
+    if x < y:
+        print(x)
+    if y < x:
+        print(y)
+def A_circulo(raio):
+    pi = 3.14
+    print(pi * (raio**2))   
 
-if a > b:
-    print(f"Maior: {a}")
-elif b > a:
-    print(f"Maior: {b}")
-else:
-    print("Números iguais")
+def Frete(m,d):
+
+Num_Menor(8,900)
+A_circulo(4)

0c656cec4e941766c827ecfbf25c0645d3f67627
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-03-15 20:30:33 +0000

    salvar delta

--- Pasta_aula4/if_else.py
+++ Pasta_aula4/if_else.py
@@ -2,15 +2,13 @@ bas = input().split()
 a = float(bas[0])
 b = float(bas[1])
 c = float(bas[2])
-
-if a <= 0:
-    print("Impossivel calcular")
-    
-    
 delta = (b**2) - (4*a*c)
 
+if a == 0 or delta < 0:
+    print("Impossivel calcular")
 
-r1 = ((-b) + (delta**0.5))/(2*a)
-r2 = ((-b) - (delta**0.5))/(2*a)
-print(f"R1 = {r1:.5f}")
-print(f"R2 = {r2:.5f}")
+else:
+    r1 = ((-b) + (delta**0.5))/(2*a)
+    r2 = ((-b) - (delta**0.5))/(2*a)
+    print(f"R1 = {r1:.5f}")
+    print(f"R2 = {r2:.5f}")

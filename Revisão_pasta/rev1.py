8318d22011fa63a735b2abc3fc27d5d3b37b1aa8
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-03-18 23:35:20 +0000

    salvar revisão

--- Revisão_pasta/rev1.py
+++ Revisão_pasta/rev1.py
@@ -1,14 +1,1 @@
-#Função: Ultimo dia do mês 
-def Ultmes(mes,ano):
-    if mes == 1 or mes == 3:
-        print(f"31/0{mes}/{ano}")
-    if mes == 5 or mes == 7:
-        print(f"31/0{mes}/{ano}")
-    if mes == 8 or mes == 10 or mes == 12:
-        print(f"31/0{mes}/{ano}")
-    if mes == 2:
-        print(f"28/0{mes}/{ano}")
-    else:
-        print(f"30/0{mes}/{ano}")
-
-Ultmes(8,2007)
+print(1 + 2 + 3 * 4)

3ff11f3fcef643ba28b89edb6d9c897f19ff817e
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-04-02 17:57:04 +0000

    junior

--- Pasta_aula6/classes_estudantes.py
+++ Pasta_aula6/classes_estudantes.py
@@ -1,3 +1,12 @@
-class Mouse:
+class Triangulo:
     def __init__(self):
-        pass
+        self.h = 0
+        self.b = 0
+    
+    def Area_tri(self):
+        return (self.b * self.h)/2
+
+
+junior = Triangulo()
+
+junior.Area_tri()

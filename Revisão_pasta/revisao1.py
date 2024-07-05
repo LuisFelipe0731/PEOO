754e8bf663175d55c0dfa8f683a5377730ca21dc
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-05 19:55:41 +0000

    salvar

--- Revisão_pasta/revisao1.py
+++ Revisão_pasta/revisao1.py
@@ -1,2 +1,15 @@
 #aula 11 - revisão
 #re-introdução à POO
+class triangulo: #Classe
+    def __init__(self):
+        self.b = 0
+        self.h = 0
+    def calc_area(self):
+        return self.b * self.h/2
+    
+ju = triangulo() #Objeto
+
+ju.h = 3
+ju.b = 5
+
+print(ju.calc_area())

d244ee9ef615d14545445b501029cc14b9f26bb2
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-09 17:57:08 +0000

    salvar

--- Revisão_pasta/ecapsulamento.py
+++ Revisão_pasta/ecapsulamento.py
@@ -1,1 +1,7 @@
 #Encapsulamento - aula 12
+class Triangulo:
+    def __init__(self):
+        self.b = 0  #atributos
+        self.h = 0
+    def calc_area(self): #metodo
+        return self.b * self.h / 2

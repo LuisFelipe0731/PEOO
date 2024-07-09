50b294a264deb0f93ade36ef4d2e8b63cbc7826d
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-09 18:14:08 +0000

    cazuza2

--- Revisão_pasta/ecapsulamento.py
+++ Revisão_pasta/ecapsulamento.py
@@ -3,11 +3,11 @@ class Triangulo: #classe entidade
     
     def __init__(self): #Método construtor
         
-        self.b = 0  #atributos
-        self.h = 0
+        self.__b = 0  #atributos encapsulados
+        self.__h = 0
     
     def calc_area(self): #método
-        return self.b * self.h / 2
+        return self.__b * self.__h / 2
     
 class UI: #Classe de entrada e saida
     

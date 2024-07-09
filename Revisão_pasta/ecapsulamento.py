18b1c6ada7df6767868766188126b8b4c3210331
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-09 18:02:42 +0000

    salvaAAAAAAAH

--- Revisão_pasta/ecapsulamento.py
+++ Revisão_pasta/ecapsulamento.py
@@ -1,7 +1,17 @@
 #Encapsulamento - aula 12
-class Triangulo:
+class Triangulo: #classe entidade
     def __init__(self):
         self.b = 0  #atributos
         self.h = 0
     def calc_area(self): #metodo
         return self.b * self.h / 2
+    
+class UI: #Classe de entrada e saida
+    @staticmethod
+    def main():
+        x = Triangulo()
+        x.b = 10
+        x.h = 20
+        print(x.calc_area())
+    
+UI.main()

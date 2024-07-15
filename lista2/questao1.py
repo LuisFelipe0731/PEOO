5dc686c67998e941e4563621762ee7e668c9ee2d
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-15 23:27:35 +0000

    salvar

--- lista2/questao1.py
+++ lista2/questao1.py
@@ -1,6 +1,24 @@
 class Circulo:
     def __init__(self):
-        
+        self.__raio = 0
+        self.__pi = 3.14
+    
+    def set_raio(self,valor):
+        if valor > 0:
+            self.__raio = valor
+        else:
+            raise ValueError
+    
+    def get_raio(self):
+        return self.__raio
+    
+    def Area_circ(self):
+        return (self.__pi *(self.__raio**2))
+
+    def Circunferencia(self):
+        return (2 * self.__pi * self.__raio)
+
+
 
 
 class UI:

cfb97b9719c9cd6ef907e7f1926e7591acaadb16
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-16 12:29:08 +0000

    salvar

--- lista2/questao1.py
+++ lista2/questao1.py
@@ -1,11 +1,26 @@
 class Viagem:
     def __init__(self):
-        self.km = 0
-        self.horas = 0
-        self.minutos = 0
-    def Velocidade(self):
-        t = self.horas + (self.minutos/60)
-        return self.km/t
+        self.__km = 0
+        self.__horas = 0
+        self.__minutos = 0
+    def set_km(self, distancia):
+        if distancia > 0:
+            self.__km = distancia
+        else:
+            raise ValueError
+    def get_km(self):
+        return self.__km
+    def set_tempo(self, tempo):
+        t = tempo.split()
+        self.__horas = int(t[0])
+        self.__minutos = int(t[1])
+        if self.__horas < 0 or self.__minutos < 0 or self.__horas + self.__minutos == 0:
+            raise ValueError
+    def get_tempo(self):
+        return f"{self.__horas}:{self.__minutos}"
+    def Velocidade_media(self):
+        tt = self.__horas + (self.__minutos/60)
+        return self.__km/tt
 
 
 
@@ -16,9 +31,11 @@ class UI:
     def main():
         x = Viagem() 
         
-        x.set_raio(float(input("Informe o valor do raio: ")))
+        x.set_km(float(input("Informe a distantica da viagem: ")))
+        x.set_tempo(input("Informe o tempo da viagem: "))
+        print(x.get_km())
+        print(x.Velocidade_media())
        
-        print(x.Area_circ())
-        print(x.Circunferencia())
+        
     
 UI.main()

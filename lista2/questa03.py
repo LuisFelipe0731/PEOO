2c804ce11f18b4140bc8a1cdfff3e77b7c001c10
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-16 12:45:02 +0000

    salvar

---
+++ lista2/questa03.py
@@ -0,0 +1,9 @@
+class Conta_banco:
+    def __init__(self):
+        self.__titular = ''
+        self.__numero_conta = 0
+        self.__saldo = 0
+    
+    def set_titular(self, nome):
+        
+        

96b4f6fa3ec4fc8e1e2fec610e591076be117b69
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-22 12:01:36 +0000

    salvar

--- lista2/questa03.py
+++ lista2/questa03.py
@@ -22,7 +22,51 @@ class Conta_banco:
     def saque(self, saque):
         if saque >= 0:
             self.__saldo -= saque
+
+class Viagem2:
+    def __init__(self):
+        self.__destino = ''
+        self.__distancia = 0
+        self.__litros = 0
+    
+    def set_destino(self, local): #localização
+        self.__destino = local
+        if local == '':
+            raise ValueError
+    def get_destino(self):
+        return self.__destino
+
+    def set_distancia(self, km): #distancia
+        if km > 0:
+            self.__distancia = km
+        else:
+            raise ValueError
+    def get_distancia(self):
+        return self.__distancia
+        
+    def set_litros(self, litro): #litros
+        if litro > 0:
+            self.__litros = litro
+        else:
+            raise ValueError
+    def get_litros(self):
+        return self.__litros
+
+    def Calculo(self):
+        return self.__distancia / self.__litros
+
+class UI:
+    @staticmethod
+    def main():
+        x = Viagem2()
+       
+        x.set_destino((input("Digite seu destino: ")))
+        x.set_distancia(int(input("Digite a distancia: ")))
+        x.set_litros(int(input("Digite litros: ")))
+
+        print(x.Calculo())
     
          
 
         
+UI.main()

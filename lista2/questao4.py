a62bcb7efb4ce060fb6fefc9e905ac5704dba508
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-07-24 23:44:55 +0000

    salvar

---
+++ lista2/questao4.py
@@ -0,0 +1,23 @@
+class Retagulo:
+    def __init__(self,b,h):
+        self.__b = 0
+        self.__h = 0
+        self.set_base(b)
+        self.set_altura(h)
+    
+    def set_base(self,z):
+        if z > 0:
+            self.__b = z
+        else:
+            raise ValueError
+    
+    def get_base(self):
+        return self.__b
+
+    def set_altura(self,a):
+        if a > 0:
+            self.__h = a
+        else:
+            raise ValueError
+    def get_altura(self):
+        return self.__h

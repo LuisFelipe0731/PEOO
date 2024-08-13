10153bbe8b88faccb0aba3b76157bae80f099d23
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-13 18:50:12 +0000

    salvar

--- pasta_aula10/ifornatica2.py
+++ pasta_aula10/ifornatica2.py
@@ -1,3 +1,18 @@
-a = 1
-a = a << 2
-print(a)
+import enum
+
+class Pagamento(enum.Enum):
+    EmAberto = 1
+    ParcialPago = 2
+    Pago = 3
+
+class Boleto:
+    def __init__(self,cod_barras, v_boleto, v_pago, data1, data2, data3, pagamento):
+        self.__cod_barras = cod_barras
+        self.__v_boleto = v_boleto
+        self.__v_pago = v_pago
+        self.__data_emissao = data1
+        self.__data_venci = data2
+        self.__data_pagamento = data3
+        self.__pagamento = pagamento
+    
+    def

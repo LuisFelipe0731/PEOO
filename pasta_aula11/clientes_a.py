7cc053125aa7f87897c98735006f9736bc2e2e16
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-20 17:48:42 +0000

    salvar

--- pasta_aula11/clientes_a.py
+++ pasta_aula11/clientes_a.py
@@ -6,3 +6,13 @@ class Cliente:
         self.fone = fone
     def __str__(self):
         return f"{self.nome} - {self.id} - {self.email} - {self.fone}"    
+
+class Clientes:
+    def __init__(self):
+        self.objetos = []
+
+    def Inserir (self, cliente):
+        self.objetos.append(cliente)
+    
+    def Listar (self):
+        return self.objetos[:]

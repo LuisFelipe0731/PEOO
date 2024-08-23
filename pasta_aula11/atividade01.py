778bc98a4bbca741929cc0ce8c642e007a85e495
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-23 20:13:13 +0000

    salvar

--- pasta_aula11/atividade01.py
+++ pasta_aula11/atividade01.py
@@ -34,90 +34,120 @@ class Serviço:
 
 #listas de Objetos
 class Clientes:
-  objetos = []  
-  @classmethod
-  def inserir(cls, obj):
-    cls.abrir()
-    m = 0                     
-    for c in cls.objetos:     
-      if c.id > m: m = c.id   
-    obj.id = m + 1  
-    cls.objetos.append(obj)
-    cls.salvar()
-  
-  @classmethod
-  def listar(cls):
-    cls.abrir()
-    return cls.objetos
-  
-  @classmethod
-  def listar_id(cls, id):
-    cls.abrir()
-    for c in cls.objetos:
-      if c.id == id: return c
-    return None 
-  
-  @classmethod
-  def atualizar(cls, obj):
-    c = cls.listar_id(obj.id)
-    if c != None:
-       c.nome = obj.nome
-       c.email = obj.email
-       c.fone = obj.fone
-    cls.salvar()   
-  
-  @classmethod
-  def excluir(cls, obj):
-    c = cls.listar_id(obj.id)
-    if c != None: 
-      cls.objetos.remove(c)
-      cls.salvar()   
-  
-  @classmethod
-  def salvar(cls):  
-    with open("clientes.json", mode = "w") as arquivo:   # write
-        json.dump(cls.objetos, arquivo, default = vars) 
-  
-  @classmethod
-  def abrir(cls):
-    cls.objetos = []
-    try: 
-      with open("clientes.json", mode = "r") as arquivo:   # read
-        texto = json.load(arquivo)
-        for obj in texto:
-          c = Cliente(obj["id"], obj["nome"], obj["email"], obj["fone"])                    
-          cls.objetos.append(c)
-    except FileNotFoundError:
-      pass
-
-class Horarios:
-    horarios = []
+    objetos = []  
     @classmethod
     def inserir(cls, obj):
         cls.abrir()
-        n = 0                     
-        for h in cls.horarios:     
-            if h.id > n:
-               n = h.id   
-        obj.id = n + 1  
-        cls.horarios.append(obj)
+        m = 0                     
+        for c in cls.objetos:     
+            if c.id > m: m = c.id   
+        obj.id = m + 1  
+        cls.objetos.append(obj)
         cls.salvar()
     
     @classmethod
+    def listar(cls):
+        cls.abrir()
+        return cls.objetos
+    
+    @classmethod
+    def listar_id(cls, id):
+        cls.abrir()
+        for c in cls.objetos:
+            if c.id == id: return c
+        return None 
+    
+    @classmethod
+    def atualizar(cls, obj):
+        c = cls.listar_id(obj.id)
+        if c != None:
+            c.nome = obj.nome
+        c.email = obj.email
+        c.fone = obj.fone
+        cls.salvar()   
+    
+    @classmethod
+    def excluir(cls, obj):
+        c = cls.listar_id(obj.id)
+        if c != None: 
+            cls.objetos.remove(c)
+        cls.salvar()   
+    
+    @classmethod
     def salvar(cls):  
-        with open("Horarios.json", mode = "w") as arquivo2:   # write
-            json.dump(cls.horarios, arquivo2, default = vars) 
-  
+        with open("clientes.json", mode = "w") as arquivo:   
+            json.dump(cls.objetos, arquivo, default = vars) 
+    
     @classmethod
     def abrir(cls):
-        cls.horarios = []
+        cls.objetos = []
         try: 
-            with open("Horarios.json", mode = "r") as arquivo2:   # read
-                texto1 = json.load(arquivo2)
-                for obj in texto1:
-                    h = Horario(obj["id"], obj["data"], obj["comfirmado"])                    
-                    cls.horarios.append(h)
+            with open("clientes.json", mode = "r") as arquivo:  
+                texto = json.load(arquivo)
+                for obj in texto:
+                    c = Cliente(obj["id"], obj["nome"], obj["email"], obj["fone"])                    
+                    cls.objetos.append(c)
         except FileNotFoundError:
             pass
-  
 
+    class Horarios:
+        horarios = []
+        @classmethod
+        def inserir(cls, obj):
+            cls.abrir()
+            n = 0                     
+            for h in cls.horarios:     
+                if h.id > n:
+                    n = h.id   
+            obj.id = n + 1  
+            cls.horarios.append(obj)
+            cls.salvar()
+        
+        @classmethod
+        def salvar(cls):  
+            with open("Horarios.json", mode = "w") as arquivo2:  
+                json.dump(cls.horarios, arquivo2, default = vars) 
+    
+        @classmethod
+        def abrir(cls):
+            cls.horarios = []
+            try: 
+                with open("Horarios.json", mode = "r") as arquivo2:   
+                    texto1 = json.load(arquivo2)
+                    for obj in texto1:
+                        h = Horario(obj["id"], obj["data"], obj["comfirmado"])                    
+                        cls.horarios.append(h)
+            except FileNotFoundError:
+                pass
+    
+class Serviços:
+    serv = []
+   
+    @classmethod
+    def inserir(cls, obj):
+        cls.abrir()
+        x = 0                     
+        for s in cls.horarios:     
+            if s.id > x:
+                x = s.id   
+        obj.id = x + 1  
+        cls.serv.append(obj)
+        cls.salvar()
+        
+    @classmethod
+    def salvar(cls):  
+            with open("Serviços.json", mode = "w") as arquivo3:  
+                json.dump(cls.horarios, arquivo3, default = vars) 
+    
+    @classmethod
+    def abrir(cls):
+            cls.horarios = []
+            try: 
+                with open("Serviços.json", mode = "r") as arquivo3:   
+                    texto2 = json.load(arquivo3)
+                    for obj in texto2:
+                        s = Horario(obj["id"], obj["data"], obj["comfirmado"])                    
+                        cls.horarios.append(s)
+            except FileNotFoundError:
+                pass
+    

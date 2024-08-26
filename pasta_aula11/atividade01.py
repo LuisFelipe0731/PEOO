85d348a879bdfc03487b8b223ada36e633908452
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-26 12:21:22 +0000

    salvar

--- pasta_aula11/atividade01.py
+++ pasta_aula11/atividade01.py
@@ -152,54 +152,59 @@ class Serviços:
 
 
 class UI:
-  @staticmethod
-  def menu():
-    print("1 - Inserir cliente, 2 - listar clientes, 3 - atualizar cliente, 4 - excluir cliente")
-    print("5 - Inserir Horario, 6 - Listar Horarios ,7,8")
-    print("9,10,11,12,13")
+    #Menu
+    @staticmethod
+    def menu():
+        print("1 - Inserir cliente, 2 - listar clientes, 3 - atualizar cliente, 4 - excluir cliente")
+        print("5 - Inserir Horario, 6 - Listar Horarios ,7,8")
+        print("9,10,11,12,13")
 
-    return int(input("Informe uma opção: "))
+        return int(input("Informe uma opção: "))
+    #Main
+    @staticmethod
+    def main():
+        op = 0
+        while op != 13: 
+            op = UI.menu()
+            if op == 1: UI.cliente_inserir()
+            if op == 2: UI.cliente_listar()
+            if op == 3: UI.cliente_atualizar()
+            if op == 4: UI.cliente_excluir()
+    
+    #Clientes
+    @staticmethod
+    def cliente_inserir():
+        id = int(input("Informe o id: "))
+        nome = input("Informe o nome: ")
+        email = input("Informe o e-mail: ")
+        fone = input("Informe o fone: ")
+        c = Cliente(id, nome, email, fone)
+        Clientes.inserir(c)
 
-  @staticmethod
-  def main():
-    op = 0
-    while op != 13: 
-      op = UI.menu()
-      if op == 1: UI.cliente_inserir()
-      if op == 2: UI.cliente_listar()
-      if op == 3: UI.cliente_atualizar()
-      if op == 4: UI.cliente_excluir()
+    @staticmethod
+    def cliente_listar():
+        for c in Clientes.listar():
+            print(c)
 
-  @staticmethod
-  def cliente_inserir():
-    id = int(input("Informe o id: "))
-    nome = input("Informe o nome: ")
-    email = input("Informe o e-mail: ")
-    fone = input("Informe o fone: ")
-    c = Cliente(id, nome, email, fone)
-    Clientes.inserir(c)
+    @staticmethod
+    def cliente_atualizar():
+        UI.cliente_listar()
+        id = int(input("Informe o id do cliente a ser atualizado: "))
+        nome = input("Informe o novo nome: ")
+        email = input("Informe o novo e-mail: ")
+        fone = input("Informe o novo fone: ")
+        c = Cliente(id, nome, email, fone)
+        Clientes.atualizar(c)
 
-  @staticmethod
-  def cliente_listar():
-    for c in Clientes.listar():
-      print(c)
-
-  @staticmethod
-  def cliente_atualizar():
-    UI.cliente_listar()
-    id = int(input("Informe o id do cliente a ser atualizado: "))
-    nome = input("Informe o novo nome: ")
-    email = input("Informe o novo e-mail: ")
-    fone = input("Informe o novo fone: ")
-    c = Cliente(id, nome, email, fone)
-    Clientes.atualizar(c)
-
-  @staticmethod
-  def cliente_excluir():
-    UI.cliente_listar()
-    id = int(input("Informe o id do cliente a ser excluído: "))
-    c = Cliente(id, "", "", "")
-    Clientes.excluir(c)
+    @staticmethod
+    def cliente_excluir():
+        UI.cliente_listar()
+        id = int(input("Informe o id do cliente a ser excluído: "))
+        c = Cliente(id, "", "", "")
+        Clientes.excluir(c)
+    
+    #Horarios
+    #Serviços
 
 UI.main()    
 

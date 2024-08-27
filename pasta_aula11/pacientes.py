7405b6066b222d267f3febca8acd8cf6519aea78
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-27 17:51:07 +0000

    salvar

--- pasta_aula11/pacientes.py
+++ pasta_aula11/pacientes.py
@@ -4,13 +4,20 @@ import datetime
 #classe paciente
 class Paciente:
     def __init__(self,id,nome,fone,nasc):
-        self.id = id
-        self.nome = nome
-        self.fone = fone
-        self.nasc = nasc
+        self.__id = 0
+        self.__nome = ""
+        self.__fone = ""
+        self.__nasc = ""
+        self.set_id(id)
+        self.set_nome(nome)
+        self.set_fone(fone)
+        self.set_nasc(nasc)
     
     def __str__(self):
         return f"{self.id} - {self.nome} - {self.fone} - {self.nasc}"
     
+class Pacientes:
+
+class UI:
         
         

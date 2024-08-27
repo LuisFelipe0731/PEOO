b3791f76f5566ff5e0907117bed44237cf069391
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-27 17:47:41 +0000

    salvar

---
+++ pasta_aula11/pacientes.py
@@ -0,0 +1,16 @@
+import json
+import datetime
+
+#classe paciente
+class Paciente:
+    def __init__(self,id,nome,fone,nasc):
+        self.id = id
+        self.nome = nome
+        self.fone = fone
+        self.nasc = nasc
+    
+    def __str__(self):
+        return f"{self.id} - {self.nome} - {self.fone} - {self.nasc}"
+    
+        
+        

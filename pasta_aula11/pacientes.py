572b414097729ed31052adc180b455b4c96f880f
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-27 17:55:01 +0000

    salvar

--- pasta_aula11/pacientes.py
+++ pasta_aula11/pacientes.py
@@ -12,6 +12,20 @@ class Paciente:
         self.set_nome(nome)
         self.set_fone(fone)
         self.set_nasc(nasc)
+
+    def set_id(self,x):
+        if x > 0: self.__id = x
+        else: raise ValueError
+    def set_nome(self,n):
+            if n != "": self.__nome = n
+            else: raise ValueError
+    def set_fone(self,f):
+            if f != "": self.__fone = f
+            else: raise ValueError
+    def set_nasc(self,data):
+            if data != "": self.__data = data
+            else: raise ValueError
+    
     
     def __str__(self):
         return f"{self.id} - {self.nome} - {self.fone} - {self.nasc}"

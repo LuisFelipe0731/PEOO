9cfc121b3a19eed56aaaefdd66b12c77a3b84fbe
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-30 19:54:16 +0000

    salvar

--- pasta_aula11/pacientes.py
+++ pasta_aula11/pacientes.py
@@ -22,5 +22,55 @@ class Pacientes: #Lista de Objetos
     pacientes = []
 
     @classmethod
-    def inserir(cls,obj):
+    def inserir(cls, obj):
         cls.abrir()
+        m = 0                     # cálculo do maior id utilizado - começa com 0
+        for c in cls.objetos:     # percorre a lista de clientes - c é cada cliente
+            if c.id > m: m = c.id   # compara o id de c com m (maior)
+        obj.id = m + 1  
+        cls.objetos.append(obj)
+        cls.salvar()
+    @classmethod
+    def listar(cls):
+    cls.abrir()
+    return cls.objetos
+  @classmethod
+  def listar_id(cls, id):
+    cls.abrir()
+    for c in cls.objetos:
+      if c.id == id: return c
+    return None 
+  @classmethod
+  def atualizar(cls, obj):
+    c = cls.listar_id(obj.id)
+    if c != None:
+       c.data = obj.data
+       c.confirmado = obj.confirmado
+       c.id_cliente = obj.id_cliente
+       c.id_servico = obj.id_servico
+    cls.salvar()   
+  @classmethod
+  def excluir(cls, obj):
+    c = cls.listar_id(obj.id)
+    if c != None: 
+      cls.objetos.remove(c)
+      cls.salvar()   
+  @classmethod
+  def salvar(cls):
+    with open("horarios.json", mode = "w") as arquivo:   # write
+        json.dump(cls.objetos, arquivo, default = Horario.to_json) 
+  @classmethod
+  def abrir(cls):
+    cls.objetos = []
+    try: 
+      with open("horarios.json", mode = "r") as arquivo:   # read
+        texto = json.load(arquivo)
+        for obj in texto:
+            c = Horario(obj["id"], datetime.strptime(obj["data"], "%d/%m/%Y %H:%M"))
+            c.confirmado = obj["confirmado"]
+            c.id_cliente = obj["id_cliente"]
+            c.id_servico = obj["id_servico"]
+            cls.objetos.append(c)
+    except FileNotFoundError:
+      pass
+    

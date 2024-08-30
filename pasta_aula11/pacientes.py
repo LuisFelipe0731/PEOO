89309e39e0cee23ccf295d2e311dfbcb5ebdab12
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-08-30 19:52:20 +0000

    salvar

--- pasta_aula11/pacientes.py
+++ pasta_aula11/pacientes.py
@@ -10,4 +10,17 @@ class Paciente:
         self.nasc = nasc
     def __str__(self):
         return f"{self.id} - {self.nome} - {self.fone} - {self.nasc.strftime('%d/%m/%M')}"
-        
+    def to_json(self):
+        dic = {}
+        dic["id"] = self.id  
+        dic["nome"] = self.nome 
+        dic["fone"] = self.fone 
+        dic["nascimento"] = self.nasc   
+        return dic
+
+class Pacientes: #Lista de Objetos
+    pacientes = []
+
+    @classmethod
+    def inserir(cls,obj):
+        cls.abrir()

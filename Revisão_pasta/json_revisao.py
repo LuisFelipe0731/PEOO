be6bd7aceae1be7b0346f46d217799d27ee71ec2
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-09-03 12:41:23 +0000

    salvar

--- Revisão_pasta/json_revisao.py
+++ Revisão_pasta/json_revisao.py
@@ -1,2 +1,11 @@
 import json
 import datetime
+
+class Aluno: #objeto
+    def __init__(self,matri,nome,curso,data):
+        self.matricula = matri
+        self.nome = nome
+        self.curso = curso
+        self.data_de_matricula = data
+    def __str__(self):
+        

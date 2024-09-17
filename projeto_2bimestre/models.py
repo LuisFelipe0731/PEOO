22b1380befdb8cd14e6570be4323c40ed421e98a
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-09-17 17:51:39 +0000

    [´po´poioiuy

--- projeto_2bimestre/models.py
+++ projeto_2bimestre/models.py
@@ -1,12 +1,23 @@
 #classes
 #Professor
 class Professsor:
-    def __init__(self, id, nome, curso, materia):
+    def __init__(self, id, nome, diretoria, materia):
+        self.id = id
+        self.nome = nome
+        self.diretoria = diretoria
+        self.materia = materia
 
 #Curso
 class Curso:
     def __init__(self, id, nome, diretoria, tempo_de_conclusao):
+        self.id = id
+        self.nome = nome
+        self.diretoria = diretoria
+        self.t = tempo_de_conclusao
 
 #Diretoria
 class Diretoria:
     def __init__(self, id, nome, cursos):
+        self.id = id
+        self.nome = nome
+        self.cursos = cursos

cf5b377ea53a569baca8127c0646b96966c13e60
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-09-17 18:29:27 +0000

    salvar

--- projeto_2bimestre/models.py
+++ projeto_2bimestre/models.py
@@ -164,3 +164,62 @@ class Cursos:
                 cls.cursos.append(c)
         except FileNotFoundError:
             pass
+
+#Diretoria
+class Professores:
+    professores = []
+    
+    @classmethod
+    def inserir(cls, obj):
+        cls.abrir()
+        m = 0                     
+        for c in cls.professores:     
+            if c.id > m: m = c.id   
+        obj.id = m + 1  
+        cls.professores.append(obj)
+        cls.salvar()
+    
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        return cls.professores
+    
+    @classmethod
+    def listar_id(cls, id):
+        cls.abrir()
+        for c in cls.professores:
+            if c.id == id: return c
+        return None 
+    
+    @classmethod
+    def atualizar(cls, obj):
+        c = cls.listar_id(obj.id)
+        if c != None:
+            c.nome = obj.nome          
+            c.diretoria = obj.diretoria
+            c.materia = obj.materia     
+        cls.salvar()   
+    
+    @classmethod
+    def excluir(cls, obj):
+        c = cls.listar_id(obj.id)
+        if c != None: 
+            cls.professores.remove(c)
+        cls.salvar()   
+    
+    @classmethod
+    def salvar(cls):  
+        with open("professores.json", mode = "w") as arquivo:   
+            json.dump(cls.professores, arquivo, default = vars) 
+    
+    @classmethod
+    def abrir(cls):
+        cls.professores = []
+        try: 
+            with open("professores.json", mode = "r") as arquivo:  
+                texto = json.load(arquivo)
+                for obj in texto:
+                    c = Professsor(obj["id"], obj["nome"], obj["diretoria"], obj["materia"])             
+                cls.professores.append(c)
+        except FileNotFoundError:
+            pass

fd21795b9df2bd3bc6444a0e4f9cd53299a56cd1
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-09-17 17:47:38 +0000

    salvar

---
+++ projeto_2bimestre/models.py
@@ -0,0 +1,12 @@
+#classes
+#Professor
+class Professsor:
+    def __init__(self, id, nome, curso, materia):
+
+#Curso
+class Curso:
+    def __init__(self, id, nome, diretoria, tempo_de_conclusao):
+
+#Diretoria
+class Diretoria:
+    def __init__(self, id, nome, cursos):

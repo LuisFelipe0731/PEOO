598e0a6218f70886538205bd87f16362fa3b0d7c
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-09-17 17:55:33 +0000

    uytru

--- projeto_2bimestre/models.py
+++ projeto_2bimestre/models.py
@@ -6,6 +6,8 @@ class Professsor:
         self.nome = nome
         self.diretoria = diretoria
         self.materia = materia
+    def __str__(self):
+        return f""
 
 #Curso
 class Curso:
@@ -14,6 +16,17 @@ class Curso:
         self.nome = nome
         self.diretoria = diretoria
         self.t = tempo_de_conclusao
+    def __str__(self):
+        return f""
+    
+    def to_json(self):
+        dic = {}
+        
+        dic["id"] = self.id
+        dic["nome"] = self.nome
+        dic["diretoria"] = self.diretoria
+        dic["tempo de conclusâo"] = self.t.strftime('%d/%m/%Y %H:%M')
+        return dic
 
 #Diretoria
 class Diretoria:
@@ -21,3 +34,7 @@ class Diretoria:
         self.id = id
         self.nome = nome
         self.cursos = cursos
+        self.id_professor = 0
+        self.id_curso = 0
+    def __str__(self):
+        return f""

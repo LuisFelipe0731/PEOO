f920ffefa09e0a950f30ecdfc294ae2ea49bd4f8
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-09-18 23:04:02 +0000

    hurt when i piss

---
+++ projeto_2bimestre/user_interface.py
@@ -0,0 +1,13 @@
+import view
+
+#User Interface
+class UI:
+    @classmethod
+    def Menu(cls):
+        print("Professores: 1 - Inserir, 2 - listar, 3 - atualizar, 4 - excluir")
+        print("Cursos: 5 - Inserir, 6 - listar, 7 - atualizar, 8 - excluir")
+        print("Diretorias: 9 - Inserir, 10 - listar, 11 - atualizar, 12 - excluir")
+
+    def Main(cls):
+
+UI.Main()
---
+++ projeto_2bimestre/view.py
@@ -0,0 +1,48 @@
+from models import Professor, Professores, Diretoria, Diretorias, Curso, Cursos
+#view
+
+#Professores
+def Professor_inserir(nome,diretoria, materia):
+    Professores.inserir(Professor(0,nome,diretoria, materia))
+
+def Listar_professor():
+    return Professores.listar()
+
+def Atualizar_professor(id,nome, diretoria, materia):
+    c = Professor(id,nome,diretoria,materia)
+    Professores.atualizar(c)
+
+def Excluir_professor(id):
+    c = Professor(id, "", "", "")
+    Professores.excluir(c)
+
+
+#Cursos
+def curso_inserir(nome,diretoria, data):
+    Cursos.inserir(Curso(0,nome,diretoria, data))
+
+def Listar_curso():
+    return Cursos.listar()
+
+def Atualizar_curso(id, nome, diretoria, data):
+    c = Curso(id,nome,diretoria,data)
+    Cursos.atualizar(c)
+
+def Excluir_curso(id):
+    c = Curso(id, "", "", "")
+    Cursos.excluir(c)
+
+#Diretoria
+def diretoria_inserir(nome):
+    Diretorias.inserir(Diretoria(0,nome))
+
+def Listar_diretoria():
+    return Diretorias.listar()
+
+def Atualizar_diretoria(id, nome):
+    c = Diretoria(id,nome)
+    Diretorias.atualizar(c)
+
+def Excluir_diretoria(id):
+    c = Diretoria(id,"")
+    Diretorias.excluir(c)

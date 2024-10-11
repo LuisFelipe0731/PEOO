24c97c069c6cdfb75e80a54c822c1e0fa4642b04
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-10-11 20:47:15 +0000

    XWQEC

---
+++ pasta_aula12/equacao2grau.py
@@ -0,0 +1,19 @@
+import math
+
+class Equacao2grau:
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+    def __str__(self):
+        return f"A = {self.__a} B = {self.__b} C = {self.__c}"
+
+    def Delta(self):
+        return self.__b ** 2 - (4 * self.__a * self.__c)
+
+    def Tem_raiz(self):
+
+    def raiz1(self):
+
+    def raiz2(self):
+

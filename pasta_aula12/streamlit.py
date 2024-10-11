f706e4a8af4d83cecf658beae0ad1176e52b5dea
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-10-11 20:32:02 +0000

    fiend club

--- pasta_aula12/streamlit.py
+++ pasta_aula12/streamlit.py
@@ -1,6 +1,17 @@
 import streamlit as st
+from retangulo import Retangulo
 
-st.header("PEOO com o streamlit")
+class RetanguloUI:
+    def main():
+        st.header("Calculos com retangulo")
+        b = st.text_input("Digite a base: ")
+        h = st.text_input("Digite a altura: ")
+        if st.button("Calcular"):
+            r = Retangulo(float(b),float(h))
+            st.write(r)
+            st.write(f"Área = {r.calc_area()}")
+            st.write(f"Diagonal = {r.calc_diagonal()}")
 
-if st.button("Clique aqui"):
-    st.write("Hello World")
+
+
+

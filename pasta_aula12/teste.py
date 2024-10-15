95cc39d2cf65ddf829a843036c68171fc4041c88
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-10-15 17:51:47 +0000

    uurru

--- pasta_aula12/teste.py
+++ pasta_aula12/teste.py
@@ -1,7 +1,21 @@
 from equacao2grau import Equacao2grau
+import streamlit as st
 
-x = Equacao2grau(12,-9,-3)
+class EquacaoUI:
+    def main():
+        st.header("Equação do segundo grau: ")
+        a = st.text_input("Digite o valor de A: ")
+        b = st.text_input("Digite o valor de B: ")
+        c = st.text_input("Digite o valor de C: ")
+            
+        if st.button("calcular"):
+            x = Equacao2grau(int(a), int(b), int(c))
+            
+            st.write(x)
+            st.write(x.Delta())
+            
+            st.write(x.raiz1())
+            st.write(x.raiz2())
 
-print(x.Tem_raiz())
-print(x.raiz1())
-print(x.raiz2())
+EquacaoUI.main()
+

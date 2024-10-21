5201627f23094d805f061ad5b0f798c1b86caff7
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-10-21 12:20:53 +0000

    rerer

--- streamlit_cliente/view.py
+++ streamlit_cliente/view.py
@@ -1,3 +1,12 @@
 from cliente import *
 
 #funcoes view
+def clientes_inserir(nome, email, fone):
+    Clientes.inserir(Cliente(0,nome,email,fone))
+
+def listar_clientes():
+    return Clientes.listar()
+
+def clientes_atualizar(id, nome, email, fone):
+    c = Clientes(id,nome,email,fone)
+    Clientes.atualizar(c)

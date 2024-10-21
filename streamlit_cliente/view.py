f61d1c3c9f1f42bb0c0624e80b06b42da769d179
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-10-21 12:24:10 +0000

    yry

--- streamlit_cliente/view.py
+++ streamlit_cliente/view.py
@@ -8,5 +8,9 @@ def listar_clientes():
     return Clientes.listar()
 
 def clientes_atualizar(id, nome, email, fone):
-    c = Clientes(id,nome,email,fone)
+    c = Cliente(id,nome,email,fone)
     Clientes.atualizar(c)
+
+def clientes_excluir(id):
+    c = Cliente(id,"","","")
+    Clientes.excluir(c)

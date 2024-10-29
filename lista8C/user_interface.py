f4d597dcda27803514eb62b1597316ea1826eadb
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-10-29 12:11:30 +0000

    tete

--- lista8C/user_interface.py
+++ lista8C/user_interface.py
@@ -138,3 +138,59 @@ class HorarioUI:
             if st.button("Excluir"):
                 View_horario.horarios_excluir(op.id)
                 st.rerun()
+
+class ServicoUI:
+    @staticmethod
+    def main():
+        st.header("Cadastro de serviços")
+        listar, inserir, atualizar, excluir = st.tabs(["Listar","Inserir","Atualizar","Excluir"])
+
+        with listar:
+            ServicoUI.listar_servicos()
+
+        with inserir:
+            ServicoUI.inserir_servico()
+             
+        with atualizar:
+            ServicoUI.atualizar_servico()
+        
+        with excluir:
+            ServicoUI.excluir_servico()
+
+    @staticmethod
+    def listar_clientes():
+        lista = View_cliente.listar_clientes()
+        if len(lista) == 0:
+            st.write("Nenhum cliente foi adicionado ainda.")
+        
+        else:
+            dic = []
+            for c in lista:
+                dic.append(c.__dict__)
+            df = pd.DataFrame(dic)
+            st.dataframe(df)
+    
+    def inserir_cliente():
+        nome = st.text_input("Informe o nome: ")
+        email = st.text_input("Informe o email: ")
+        fone = st.text_input("Informe o telefone: ")
+        if st.button("Inserir"):
+            View_cliente.clientes_inserir(nome, email, fone)
+            st.rerun()
+    
+    @staticmethod
+    def atualizar_cliente():
+        lista = View_cliente.listar_clientes()
+        if len(lista) == 0:
+            st.write("Nenhum cliente foi adicionado ainda.")
+        
+        else:
+            op = st.selectbox("atualização de clientes", lista)
+            nome = st.text_input("Informe o novo nome: ", op.nome)
+            email = st.text_input("Informe o novo email: ", op.email)
+            fone = st.text_input("Informe o novo telefone: ", op.fone)
+            
+            if st.button("Atualizar"):
+                View_cliente.clientes_atualizar(op.id ,nome, email, fone)
+                st.rerun()
+

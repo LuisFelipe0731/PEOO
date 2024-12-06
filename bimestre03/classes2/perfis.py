a8e393025b5d32a3f05e694123efb4c71e0c2a72
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-12-06 11:50:38 +0000

    salvar

--- bimestre03/classes2/perfis.py
+++ bimestre03/classes2/perfis.py
@@ -10,4 +10,30 @@ class Perfil:
     def __str__(self):
         return f"{self.nome} - {self.desc} - {self.beneficio}"
 
-class Perfis(CRUD)
+class Perfis(CRUD):
+    @classmethod
+    def atualizar(cls, obj):
+        c = cls.listar_id(obj.id)
+        if c != None:
+            c.nome = obj.nome
+            c.desc = obj.desc
+            c.beneficio = obj.beneficio
+            
+        cls.salvar()
+    
+    @classmethod
+    def salvar(cls):
+        with open("perfis.json", mode="w") as arquivo:   # w - write
+            json.dump(cls.objetos, arquivo, default = vars)
+
+    @classmethod
+    def abrir(cls):
+        cls.objetos = []
+        try:
+            with open("perfis.json", mode="r") as arquivo:   # r - read
+                texto = json.load(arquivo)
+                for obj in texto:   
+                    c = Perfil(obj["id"], obj["nome"], obj["desc"], obj["beneficio"])
+                    cls.objetos.append(c)
+        except FileNotFoundError:
+            pass

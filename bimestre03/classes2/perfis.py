3a703788f4118e7ce7f0172019123c3739b1f321
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2024-12-06 11:45:16 +0000

    salvar

--- bimestre03/classes2/perfis.py
+++ bimestre03/classes2/perfis.py
@@ -1,2 +1,13 @@
 from crud import CRUD
 import json
+
+class Perfil:
+    def __init__(self, id, nome, desc, beneficio):
+        self.id = id
+        self.nome = nome
+        self.desc = desc
+        self.beneficio = beneficio
+    def __str__(self):
+        return f"{self.nome} - {self.desc} - {self.beneficio}"
+
+class Perfis(CRUD)

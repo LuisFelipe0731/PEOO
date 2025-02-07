77ba28e7ada201cabe41d8a98de045a7497509a9
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-07 20:01:54 +0000

    salvarhh

--- projeto_4bim/Models/livro.py
+++ projeto_4bim/Models/livro.py
@@ -9,8 +9,14 @@ class Livro():
         self.__autor = ""
         self.__data_publicacao = ""
         self._id_genero = 0
+
+        self.Set_valores(id, titulo, autor, data)
     
+    #get e set
+    def Set_valores(id, t, a, d):
+
     #str
     def __str__(self):
         return f"{self.__id} - {self.__titulo} - {self.__autor} - {self.__data_publicacao}"
-        
+    
+    #to_json

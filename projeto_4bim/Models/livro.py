e3a2dae657696ec399a0def24d4c523a2c89a4d6
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-07 19:58:39 +0000

    salva ai o santiago oh

---
+++ projeto_4bim/Models/livro.py
@@ -0,0 +1,16 @@
+import json
+from datetime import datetime
+
+#Livro
+class Livro():
+    def __init__(self,id, titulo, autor, data,):
+        self.__id = 0
+        self.__titulo = ""
+        self.__autor = ""
+        self.__data_publicacao = ""
+        self._id_genero = 0
+    
+    #str
+    def __str__(self):
+        return f"{self.__id} - {self.__titulo} - {self.__autor} - {self.__data_publicacao}"
+        

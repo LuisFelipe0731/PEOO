61db89c121d23d3956561c354507afbe39a38d4e
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-09 22:49:35 +0000

    salvar

--- projeto_4bim/Models/genero.py
+++ projeto_4bim/Models/genero.py
@@ -2,10 +2,15 @@ import json
 from crud import CRUD
 
 class Genero:
-    def __init__(self):
+    def __init__(self,id, nome, descricao):
+        self.__id = 0
+        self.__nome = ""
+        self.__desc = ""
+    #set e get
         
 
     def __str__(self):
+        return f"{self.__id} - {self.__nome} - {self.__desc}"
         
         
 class Generos(CRUD):

076dbf3ddf367a31a133e60ce024af6260db1f02
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-09 23:02:17 +0000

    nirvana

--- projeto_4bim/Models/exemplar.py
+++ projeto_4bim/Models/exemplar.py
@@ -2,5 +2,18 @@ import json
 from crud import CRUD
 
 class Exemplar:
+    def __init__(self, id, edicao):
+        self.__id = 0
+        self.__ed = ""
+        self.__id_livro = 0
+    
+    #set e get 
+    
+    def __str__(self):
+        return f"{self.__id} - {self.__ed}"
+    
+    def to_json(self):
+
+        
 
 class Exemplares(CRUD):

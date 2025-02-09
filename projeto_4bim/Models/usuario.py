ac0c190454089747dc3d904c4aae37fbe3b0d396
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-09 23:09:50 +0000

    sas

--- projeto_4bim/Models/usuario.py
+++ projeto_4bim/Models/usuario.py
@@ -2,6 +2,13 @@ import json
 from crud import CRUD
 
 class Usuario:
+    def __init__(self,id,nome,email,fone,senha):
+        pass
+
+    #set e get
+
+    def __str__(self):
+        pass
 
 
 class Usuarios(CRUD):

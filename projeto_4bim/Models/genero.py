8d1532b8c9210451972cd0821c762c30d69bc6b6
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-09 22:52:41 +0000

    salvar

--- projeto_4bim/Models/genero.py
+++ projeto_4bim/Models/genero.py
@@ -6,7 +6,30 @@ class Genero:
         self.__id = 0
         self.__nome = ""
         self.__desc = ""
+    
     #set e get
+    def set_id(self,id):
+        if id != 0:
+            self.__id = id
+        else:
+            raise ValueError("Valor invalido")
+
+    def set_nome(self,n):
+        if n != "":
+            self.__nome = n
+        else:
+            raise ValueError("O genero precisa de um nome")
+    
+    def set_desc(self,d):
+        if d != "":
+            self.__desc = d
+        else:
+            raise ValueError("O genero precisa de uma descrição")
+        
+    def get_id(self): return self.__id
+    def get_nome(self): return self.__nome
+    def get_desc(self): return self.__desc
+  
         
 
     def __str__(self):

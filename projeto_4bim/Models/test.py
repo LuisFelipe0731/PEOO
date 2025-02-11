2334a55aad60abb8e79c12850b0043ed6d159560
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-11 18:11:14 +0000

    salvar

--- projeto_4bim/Models/test.py
+++ projeto_4bim/Models/test.py
@@ -6,6 +6,8 @@ from compra import *
 x = Livro(1, "O pequeno principe"," Antoine de Saint-Exupéry",'01/04/1943')
 y = Livro(2,"Dracula","Bram Stoker",'26/05/1897')
 
+a = Genero(3,"Terror","Genero que envolve medo e suspense")
+
 
 Livros.inserir(x)
 Livros.inserir(y)

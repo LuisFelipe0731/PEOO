a9f9d2f9fb8ffe3c7037d67c5211e00a84eb03f5
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-11 17:57:28 +0000

    sav

--- projeto_4bim/Models/test.py
+++ projeto_4bim/Models/test.py
@@ -1,9 +1,10 @@
 from livro import *
 
 x = Livro(1, "O pequeno Principe"," Antoine de Saint-Exupéry",'01/04/1943')
-
-print(x)
+y = Livro(2,"Dracula","Bram Stoker",'26/05/1897')
 
 Livros.inserir(x)
-Livros.salvar()
-Livros.listar()
+Livros.inserir(y)
+
+for l in Livros.listar():
+    print(l)

de27767903720cf70def8a41db6bd96996eb6114
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-25 12:09:59 +0000

    s

---
+++ projeto_4bim/Paginas/generosUI.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+from view import View
+import time
+
+class ManterGeneroUI:
+    def main():
+        st.header("Cadastro de Livros")
+        tab1, tab2, tab3, tab4 = st.tabs(["Listar", "Inserir", "Atualizar", "Excluir"])
+        with tab1: ManterGeneroUI.listar()
+        with tab2: ManterGeneroUI.inserir()
+        with tab3: ManterGeneroUI.atualizar()
+        with tab4: ManterGeneroUI.excluir()
+    
+    def listar():
+        perfis = View.Livro_listar()
+        if len(perfis) == 0: 
+            st.write("Nenhum livro cadastrado")
+        else:    
+            
+            dic = []
+            for obj in perfis: dic.append(obj.__dict__)
+            df = pd.DataFrame(dic)
+            st.dataframe(df)
+    
+    def inserir():
+        t = st.text_input("Informe o titulo do livro: ")
+        a = st.text_input("Informe o autor: ")
+        data = st.text_input("Informe a data de publicação: ")
+        if st.button("Inserir"):
+            View.Livro_inserir(t,a,data)
+            st.success("Livro inserido com sucesso")
+            time.sleep(2)
+            st.rerun()
+    
+    def atualizar():
+        livros = View.Livro_listar()
+        if len(livros) == 0: 
+            st.write("Nenhum livro cadastrado")
+        else:
+            op = st.selectbox("Atualização de livros", livros)
+            t = st.text_input("Informe o novo titulo: ", op.__titulo)
+            a = st.text_input("Informe o novo autor: ", op.__autor)
+            data = st.text_input("Informe a nova data de publicação: ", op.__data_publicacao)
+            
+            if st.button("Atualizar"):
+                View.cliente_atualizar(op.__id, t, a, data)
+                st.success("Livro atualizado com sucesso")
+                time.sleep(2)
+                st.rerun()
+    
+    def excluir():
+        objetos = View.Livro_listar()
+        if len(objetos) == 0: 
+            st.write("Nenhum livro cadastrado")
+        else:
+            op = st.selectbox("Exclusão de perfis", objetos)
+            if st.button("Excluir"):
+                View.Livro_excluir(op.__id)
+                st.success("Livro excluído com sucesso")
+                time.sleep(2)
+                st.rerun()

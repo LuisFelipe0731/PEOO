88b4a118b4a67a716c7bddf5eb5758c8ae1cbbc0
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-25 18:27:41 +0000

    aa

--- projeto_4bim/Paginas/atualizar.py
+++ projeto_4bim/Paginas/atualizar.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from view import View
+import time
 
 class AtualizarUI():
     def main():
@@ -9,5 +10,23 @@ class AtualizarUI():
         with tab2: AtualizarUI.excluir()
 
     def atualizar():
+        nome = st.text_input("Informe o nome")
+        email = st.text_input("Informe o e-mail")
+        senha = st.text_input("Informe a senha", type="password")
+        if st.button("Inserir"):
+            View.Usuario_inserir(nome, email, senha)
+            st.success("Conta criada com sucesso")
+            time.sleep(2)
+            st.rerun()
     
     def excluir():
+        objs = View.Livro_listar()
+        if len(objs) == 0: 
+            st.write("Nenhum livro cadastrado")
+        else:
+            op = st.selectbox("Exclusão de livros", objs)
+            if st.button("Excluir"):
+                View.Livro_excluir(op.__id)
+                st.success("Livro excluído com sucesso")
+                time.sleep(2)
+                st.rerun()

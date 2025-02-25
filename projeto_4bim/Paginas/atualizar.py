0e92a6c4fe1b6994d44818afab8803d7622d4d1b
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-25 18:24:40 +0000

    sa

--- projeto_4bim/Paginas/atualizar.py
+++ projeto_4bim/Paginas/atualizar.py
@@ -5,3 +5,5 @@ class AtualizarUI():
     def main():
         st.header("Editar Perfil")
         tab1, tab2 = st.tabs(["Atualizar", "Excluir"])
+        with tab1: AtualizarUI.atualizar()
+        with tab2: AtualizarUI.excluir()

40880719fdcbabfce736911a009bfec9d415ba40
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-26 12:09:21 +0000

    sa

--- projeto_4bim/Paginas/exemplaresUI.py
+++ projeto_4bim/Paginas/exemplaresUI.py
@@ -51,28 +51,27 @@ class ManterExemplarUI:
         else:
             generos = View.Genero_listar()
             op = st.selectbox("Atualização de livros", objs)
-            t = st.text_input("Informe o novo titulo: ", op.__titulo)
-            a = st.text_input("Informe o novo autor: ", op.__autor)
-            data = st.text_input("Informe a nova data de publicação (formato: dd/mm/aa): ", op.__data_publicacao.strftime("%d/%m/%Y"))
+            e = st.text_input("Informe o novo titulo: ", op.__titulo)
+            v = st.text_input("Informe o novo autor: ", op.__autor)
             id_genero = None if op.id_genero in [0, None] else op.id_genero
             genero = st.selectbox("Informe o novo genero", generos, next((i for i, c in enumerate(generos) if c.__id == id_genero), None))
             
             if st.button("Atualizar"):
                 id_genero = None
                 if id_genero != None: id_genero = genero.__id
-                View.Exemplar_atualizar(op.__id, t, a, datetime.strptime(data, "%d/%m/%Y"),id_genero)
+                View.Exemplar_atualizar(op.__id, e, float(v), id_livro)
                 st.success("Exemplar atualizado com sucesso")
                 time.sleep(2)
                 st.rerun()
     
     def excluir():
-        objs = View.Livro_listar()
+        objs = View.Exemplar_listar()
         if len(objs) == 0: 
-            st.write("Nenhum livro cadastrado")
+            st.write("Nenhum exemplar cadastrado")
         else:
-            op = st.selectbox("Exclusão de livros", objs)
+            op = st.selectbox("Exclusão de exemplares", objs)
             if st.button("Excluir"):
-                View.Livro_excluir(op.__id)
-                st.success("Livro excluído com sucesso")
+                View.Exemplar_excluir(op.__id)
+                st.success("exemplar excluído com sucesso")
                 time.sleep(2)
                 st.rerun()

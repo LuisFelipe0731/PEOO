78f0a6036bbcec90f4c0e4bcb3a70ea5b01d5e8e
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-26 11:55:40 +0000

    as

--- projeto_4bim/Models/exemplar.py
+++ projeto_4bim/Models/exemplar.py
@@ -2,13 +2,15 @@ import json
 from Models.crud import CRUD
 
 class Exemplar:
-    def __init__(self, id, edicao):
+    def __init__(self, id, edicao, valor):
         self.__id = 0
         self.__ed = ""
+        self.__valor = 0
         self.__id_livro = 0
 
         self.set_id(id)
         self.set_edicao(edicao)
+        self.set_valor(valor)
     
     #set e get 
     def set_id(self,id):
@@ -23,17 +25,25 @@ class Exemplar:
         else:
             raise ValueError("O exemplar precisa de uma edição")
     
+    def set_valor(self,s):
+        if s <= 0:
+            self.__valor = s
+        else:
+            raise ValueError("O valor precisa ser maior que 0")
+    
     def get_id(self): return self.__id
     def get_edicao(self): return self.__ed
+    def get_valor(self): return self.__valor
 
 
     def __str__(self):
-        return f"{self.__id} - {self.__ed}"
+        return f"{self.__id} - {self.__ed} - {self.__valor}"
     
     def to_json(self):
         dic = {}
         dic["id"] = self.__id  
         dic["edição"] = self.__ed
+        dic["valor"] = self.__valor
         dic["livro"] = self.__id_livro
         return dic
 
@@ -43,6 +53,7 @@ class Exemplares(CRUD):
         c = cls.listar_id(obj.id)
         if c != None:
             c.__ed = obj.__ed
+            c.__valor = obj.__valor
             c.__id_livro = obj.__id_livro
         cls.salvar()
         
@@ -59,7 +70,7 @@ class Exemplares(CRUD):
             with open("Exemplares.json", mode="r") as arquivo:   # r - read
                 texto = json.load(arquivo)
                 for obj in texto:   
-                    c = Exemplar(obj["id"],obj["edição"])
+                    c = Exemplar(obj["id"],obj["edição"],obj["valor"])
                     c.__id_livro = obj["livro"]
                     cls.objetos.append(c)
         except FileNotFoundError:

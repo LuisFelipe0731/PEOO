4c8dc8f9c44e4e6c4373ed877d7bf8f116490409
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-02-28 18:28:24 +0000

    sas

--- projeto_4bim/Paginas/login_livro.py
+++ projeto_4bim/Paginas/login_livro.py
@@ -9,9 +9,12 @@ class LoginUI:
         senha = st.text_input("Informe a senha", type="password")
         if st.button("Entrar"):
             c = View.Usuario_autenticar(email, senha)
-            if c == None: st.write("E-mail ou senha inválidos")
-            
-            if c != None:
-                st.session_state["usuario_id"] = c['id']
-                st.session_state["usuario_nome"] = c['nome']
-                st.rerun()
+            try:
+                if c == None: st.write("E-mail ou senha inválidos")
+                
+                if c != None:
+                    st.session_state["usuario_id"] = c['id']
+                    st.session_state["usuario_nome"] = c['nome']
+                    st.rerun()
+            except TypeError:
+                return f"invalido invalido"

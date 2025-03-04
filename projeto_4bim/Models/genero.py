1c31a37c2a822de753cd94eedde9d5cf56e8c8ef
LuisFelipe0731 <161077479+LuisFelipe0731@users.noreply.github.com>
2025-03-04 18:11:03 +0000

    crud aaaaaa

--- projeto_4bim/Models/genero.py
+++ projeto_4bim/Models/genero.py
@@ -3,51 +3,20 @@ from Models.crud import CRUD
 
 class Genero:
     def __init__(self,id, nome, descricao):
-        self.__id = 0
-        self.__nome = ""
-        self.__desc = ""
-
-        self.set_id(id)
-        self.set_nome(nome)
-        self.set_desc(descricao)
-    
-    #set e get
-    def set_id(self,id):
-        if id != 0:
-            self.__id = id
-        else:
-            raise ValueError("Valor invalido")
-
-    def set_nome(self,n):
-        if n != "":
-            self.__nome = n
-        else:
-            raise ValueError("O genero precisa de um nome")
-    
-    def set_desc(self,d):
-        if d != "":
-            self.__desc = d
-        else:
-            raise ValueError("O genero precisa de uma descrição")
-        
-    def get_id(self): return self.__id
-    def get_nome(self): return self.__nome
-    def get_desc(self): return self.__desc
-  
-        
-
+        self.id = id
+        self.nome = nome
+        self.desc = descricao
+     
     def __str__(self):
-        return f"{self.__id} - {self.__nome} - {self.__desc}"
-        
+        return f"{self.id} - {self.nome} - {self.desc}"
         
 class Generos(CRUD):
     @classmethod
     def atualizar(cls, obj):
-        c = cls.listar_id(obj.__id)
+        c = cls.listar_id(obj.id)
         if c != None:
-            c.__nome = obj.__nome
-            c.__desc = obj.__desc
-
+            c.nome = obj.nome
+            c.desc = obj.desc
         cls.salvar()
         
     @classmethod
